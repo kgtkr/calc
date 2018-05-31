478f1485aefbe3514352d3f5d10f0d4ec8d385d6
tkr <kgtkr.jp@gmail.com>
2018-05-31 23:37:30 +0900

    Source

--- src/source.rs
+++ src/source.rs
@@ -1,3 +1,5 @@
+use rational::Rational;
+
 pub struct Source {
   s: Vec<char>,
   pos: usize,
@@ -11,17 +13,17 @@ impl Source {
     }
   }
 
-  fn peek(&self) -> Option<char> {
+  pub fn peek(&self) -> Option<char> {
     self.s.get(self.pos).cloned()
   }
 
-  fn next(&mut self) -> Option<char> {
+  pub fn next(&mut self) -> Option<char> {
     let val = self.peek();
     self.pos += 1;
     val
   }
 
-  fn num(&mut self) -> Option<i64> {
+  pub fn number(&mut self) -> Option<Rational> {
     let mut s = String::new();
     if self.peek() == Some('-') {
       self.next();
@@ -37,6 +39,59 @@ impl Source {
       }
     }
 
-    s.parse().ok()
+    s.parse::<i64>().ok().map(Rational::from)
+  }
+
+  pub fn expr(&mut self) -> Option<Rational> {
+    let mut x = self.term()?;
+    while let Some(c) = self.peek() {
+      match c {
+        '+' => {
+          self.next();
+          x += self.term()?;
+        }
+        '-' => {
+          self.next();
+          x -= self.term()?;
+        }
+        _ => {
+          break;
+        }
+      }
+    }
+    Some(x)
+  }
+
+  pub fn term(&mut self) -> Option<Rational> {
+    let mut x = self.factor()?;
+    while let Some(c) = self.peek() {
+      match c {
+        '*' => {
+          self.next();
+          x *= self.factor()?;
+        }
+        '/' => {
+          self.next();
+          x /= self.factor()?;
+        }
+        _ => {
+          break;
+        }
+      }
+    }
+    Some(x)
+  }
+
+  pub fn factor(&mut self) -> Option<Rational> {
+    if self.peek() == Some('(') {
+      self.next();
+      let ret = self.expr();
+      if self.peek() == Some(')') {
+        self.next();
+      }
+      ret
+    } else {
+      self.number()
+    }
   }
 }
