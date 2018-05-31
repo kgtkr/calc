dc406e64293bc001c04a4b8ac371fd87e0ccdeee
tkr <kgtkr.jp@gmail.com>
2018-05-31 21:32:02 +0900

    Rationalの一部

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+use std::ops::{Add, Sub};
+use std::fmt;
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[derive(Debug)]
+enum CalcError {
+    Zero,
+    Parse,
+}
+
+struct Rational {
+    n: i64,
+    d: i64,
+}
+
+impl Rational {
+    fn new(n: i64, d: i64) -> Result<Rational, CalcError> {
+        if d == 0 {
+            Err(CalcError::Zero)
+        } else {
+            Ok(Rational { n: n, d: d })
+        }
+    }
+}
+
+impl From<i64> for Rational {
+    fn from(n: i64) -> Rational {
+        Rational::new(n, 1).unwrap()
+    }
+}
+
+impl fmt::Display for Rational {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        if self.d == 1 {
+            write!(f, "{}", self.n)
+        } else {
+            write!(f, "{}/{}", self.n, self.d)
+        }
+    }
 }
