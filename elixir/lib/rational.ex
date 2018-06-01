836474b1cb3dc1c9fa99b46eff761713fa7ffc72
tkr <kgtkr.jp@gmail.com>
2018-06-02 02:06:21 +0900

    Rational少し

--- elixir/lib/rational.ex
+++ elixir/lib/rational.ex
@@ -1,22 +1,20 @@
 defmodule Rational do
   defstruct [:n, :d]
 
-  def new(n,0),do:nil
+  def new(_,0) , do: nil
   def new(n,d) do
-    g = gcd abs(n) abs(d)
-    g = if
-      d < 0
-    do
+    g = gcd abs(n), abs(d)
+    g=if d < 0 do
       -g
     else
       g
     end
 
-    %Rational{n: n/g, d:d/g}
+    %Rational{n: n/g, d: d/g}
   end
 
-  def fromInt(n),do:new n 1
+  def fromInt(n), do: new n,1
 
-  def gcd(a,0),do:a
-  def gcd(a,b),do:gcd(b, a % b)
+  def gcd(a,0), do: a
+  def gcd(a,b), do: gcd b, rem(a,b)
 end
