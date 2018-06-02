8ed1a1d1a21738132c30c3d95bf4e67d2ac435ce
tkr <kgtkr.jp@gmail.com>
2018-06-02 10:36:02 +0900

    deps

--- elixir/lib/parser.ex
+++ elixir/lib/parser.ex
@@ -1,11 +1,13 @@
 defmodule Parser do
-  def expect([x|xs],f) do
+  def expect(<<x::bytes-size(1)>><>xs,f) do
     if f.(x) do
-      {:ok,xs}
+      xs
     else
-      {:error}
+      throw v
     end
   end
 
   def char(s,c),do: expect(s,fn x -> x==c end)
+
+  def number
 end
