5b1139a697822de064499c7d94c912a718469620
tkr <kgtkr.jp@gmail.com>
2018-06-02 10:42:13 +0900

    number

--- elixir/lib/parser.ex
+++ elixir/lib/parser.ex
@@ -1,13 +1,19 @@
 defmodule Parser do
   def expect(<<x::bytes-size(1)>><>xs,f) do
     if f.(x) do
-      xs
+      {:ok,xs}
     else
-      throw v
+      {:error,nil}
     end
   end
 
   def char(s,c),do: expect(s,fn x -> x==c end)
 
-  def number
+  def number("+"<>_),do: {:error,nil}
+  def number(s) do
+    case Integer.parse(s) do
+      :error -> {:error, nil}
+      x -> {:ok, x}
+    end
+  end
 end
