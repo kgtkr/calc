a5de134b362b7cf6f4c20af0a91108ddf4fc2c91
tkr <kgtkr.jp@gmail.com>
2018-06-02 15:45:14 +0900

    出来たけど動かん

--- elixir/lib/calc.ex
+++ elixir/lib/calc.ex
@@ -1,6 +1,15 @@
+require OK
+
 defmodule Calc do
   def main() do
-    IO.puts "Hello world"
-    IO.inspect System.argv
+    OK.with do
+      s<-case List.first System.argv do
+        nil->{:error,nil}
+        x->{:ok,x}
+      end
+      s|>Parser.expr|>Rational.to_string|>IO.puts
+    else
+      _->IO.puts("Error")
+    end
   end
 end
