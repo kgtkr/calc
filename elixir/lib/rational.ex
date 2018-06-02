02ba889d52a4e78a366f0225958f330828a32dd6
tkr <kgtkr.jp@gmail.com>
2018-06-02 09:47:04 +0900

    :nan

--- elixir/lib/rational.ex
+++ elixir/lib/rational.ex
@@ -1,7 +1,7 @@
 defmodule Rational do
   defstruct [:n, :d]
 
-  def new(_,0) , do: nil
+  def new(_,0) , do: :nan
   def new(n,d) do
     g = gcd abs(n), abs(d)
     g=if d < 0 do
@@ -18,18 +18,18 @@ defmodule Rational do
   def gcd(a,0), do: a
   def gcd(a,b), do: gcd b, rem(a,b)
 
-  def to_string(nil),do: "NaN"
+  def to_string(:nan),do: "NaN"
   def to_string(%Rational{n: n, d: 1}), do: Kernel.to_string n
 
   def add(%Rational{n: a_n, d: a_d},%Rational{n: b_n, d: b_d}), do: new(a_n * b_d + b_n * a_d, a_d * b_d)
-  def add(_,_), do: nil
+  def add(_,_), do: :nan
 
   def sub(%Rational{n: a_n, d: a_d},%Rational{n: b_n, d: b_d}), do: new(a_n * b_d - b_n * a_d, a_d * b_d)
-  def sub(_,_), do: nil
+  def sub(_,_), do: :nan
 
   def mul(%Rational{n: a_n, d: a_d},%Rational{n: b_n, d: b_d}), do: new(a_n*b_n,a_d*b_d)
-  def mul(_,_), do: nil
+  def mul(_,_), do: :nan
 
   def div(%Rational{n: a_n, d: a_d},%Rational{n: b_n, d: b_d}), do: new(a_n*b_d,a_d*b_n)
-  def div(_,_), do: nil  
+  def div(_,_), do: :nan
 end
