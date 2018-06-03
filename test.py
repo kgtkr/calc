c628c9f667cbb5275ba051c6f7f577e343beeefd
tkr <kgtkr.jp@gmail.com>
2018-06-03 19:29:20 +0900

    テストスクリプト(バグあり)

---
+++ test.py
@@ -0,0 +1,18 @@
+import os
+import os.path
+import json
+import subprocess
+
+testCases = json.load(open("test.json"))
+
+
+for lang in [x for x in os.listdir(".") if os.path.exists(f"{x}/test.json")]:
+    langJson = json.load(open(f"{lang}/test.json"))
+    print(f"#{lang}")
+    for case in testCases:
+        subprocess.run(langJson["build"], cwd=lang)
+        out = subprocess.run(
+            [case[0] if x == "$ARGS" else x for x in langJson["run"]], cwd=lang,  check=True, stdout=subprocess.PIPE).stdout.strip().decode("UTF-8")
+        if out != case[1]:
+            print(
+                f"Error:\n\tinput:{case[0]}\n\texpect:{case[1]}\n\toutput:{out}")
