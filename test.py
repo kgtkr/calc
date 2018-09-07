28134ea8d895f840b45b3e8ed6e8fefc2558448d
kgtkr <kgtkr.jp@gmail.com>
2018-09-07 22:21:03 +0900

    test

--- test.py
+++ test.py
@@ -9,7 +9,8 @@ testCases = json.load(open("test.json"))
 for lang in [x for x in os.listdir(".") if os.path.exists(f"{x}/test.json")]:
     langJson = json.load(open(f"{lang}/test.json"))
     print(f"#{lang}")
-    subprocess.run(langJson["build"], cwd=lang, stdout=subprocess.PIPE)
+    for build in langJson["builds"]:
+        subprocess.run(build, cwd=lang, stdout=subprocess.PIPE)
     for case in testCases:
         out = subprocess.run(
             [case[0] if x == "$ARGS" else x for x in langJson["run"]], cwd=lang,  check=True, stdout=subprocess.PIPE).stdout.strip().decode("UTF-8")
