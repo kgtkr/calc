c538daf418ba47ae270e17ca265bbfd4384bc097
kgtkr <kgtkr.jp@gmail.com>
2018-09-11 23:55:24 +0900

    綺麗に

--- scheme/rational.scm
+++ scheme/rational.scm
@@ -1,7 +1,6 @@
-(define rational
-    (lambda (n d)
-        (if (equal? d 0)
-            ()
-            (let ((g (gcd (abs n) (abs d))))
-                (let ((g (if (< d 0) (- g) g)))
-                    (list (/ n g) (/ d g)))))))
+(define (rational n d)
+    (if (equal? d 0)
+        ()
+        (let ((g (gcd (abs n) (abs d))))
+            (let ((g (if (< d 0) (- g) g)))
+                (list (/ n g) (/ d g))))))
