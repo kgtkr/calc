840b205e5d9778fe057c0a5adc9487b67682e6d5
kgtkr <kgtkr.jp@gmail.com>
2018-09-12 00:27:18 +0900

    rational+

--- scheme/rational.scm
+++ scheme/rational.scm
@@ -6,8 +6,19 @@
         (vector (/ n g) (/ d g))))))
 
 (define (rational->string x)
-  (if (equal? x ())
+  (if (null? x)
     "NaN"
     (if (equal? (vector-ref x 1) 1)
       (number->string (vector-ref x 0))
       (string-append (number->string (vector-ref x 0)) "/" (number->string (vector-ref x 1))))))
+
+(define (rational+ x y)
+  (if (or (null? x) (null? y))
+    ()
+    (make-rational
+      (+
+        (* (vector-ref x 0) (vector-ref y 1))
+        (* (vector-ref y 0) (vector-ref x 1)))
+      (*
+        (vector-ref x 1)
+        (vector-ref y 1)))))
