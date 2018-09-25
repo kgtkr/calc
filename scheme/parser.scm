cd0b10a217e50daec44f4512a0700c20bfef5e2e
kgtkr <kgtkr.jp@gmail.com>
2018-09-25 18:00:36 +0900

    途中まで

--- scheme/parser.scm
+++ scheme/parser.scm
@@ -1,13 +1,73 @@
+(use util.match)
+(use srfi-1)
 (define parser-error 'parser-error)
+(define (raise-parser-error)
+    (raise parser-error))
 (define (parser-error? x)
     (eq? parser-error x))
+(define (parser-error-guard f1 f2)
+    (guard (ex [(parser-error? ex) (f2)])
+        (f1)
+    )
+)
 
-;(文字リスト . 結果)|'parser-error
-;x<-f()
+; 文字リスト,...引数
+; (文字リスト . 値)
 
 (define (parser-peak s)
-    (if (null? s)
-        parser-error
-        (cons s (car s))))
+    (match s
+        [(x . _)
+            (cons s x)
+        ]
+        [()
+            (raise-parser-error)
+        ])
+    )
 
-(call/cc (lambda (k)  (k 1)))
+(define (parser-next s)
+    (match (parser-peak s)
+        [((_ . xs) . x)
+            (cons xs x)
+        ])
+)
+
+(define (parser-expect s f)
+    (match (parser-next s)
+        [(s . x)
+            (if (f x)
+                (cons s x)
+                (raise-parser-error)
+            )
+        ])
+)
+
+(define (parser-char s c)
+    (parser-expect s (lambda (x) (eq? x c)))
+)
+
+(define (parser-number s)
+    (match (parser-error-guard
+            (lambda ()
+                (match (parser-char s #\-)
+                    [(s . _)
+                        (s,-1)
+                    ])
+            )
+            (lambda () (s,1))
+        )
+        [(s . g)
+            (define (f s)
+                (parser-error-guard
+                    (lambda () (match (parser-expect s is-digit)
+                        [(s . x) (cons x (f s))]
+                    ))
+                    (lambda () ())
+                )
+            )
+        ]
+    )
+)
+
+(define (is-digit c)
+    (and (char<=? #\0 c) (char<=? c #\9))
+)
