bbe7e7f0fff6afa1d5c1f22aee77b1a8f8c88d59
kgtkr <kgtkr.jp@gmail.com>
2018-09-25 19:40:05 +0900

    ミス

--- scheme/parser.scm
+++ scheme/parser.scm
@@ -85,12 +85,13 @@
     )
 )
 
-(define (eof s)
+(define (parser-eof s)
     (if (null? s)
         (cons s ())
         (raise-parser-error)
     )
 )
+
 
 (define (is-digit c)
     (and (char<=? #\0 c) (char<=? c #\9))
