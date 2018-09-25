7a48ec8cb7653844ea9082983040de0c77114de2
kgtkr <kgtkr.jp@gmail.com>
2018-09-26 03:46:52 +0900

    Applicative

--- haskell/app/Parser.hs
+++ haskell/app/Parser.hs
@@ -5,3 +5,9 @@ data Parser a = Parser String a|ParseError
 instance Functor Parser where
     fmap f (Parser s x) = Parser s (f x)
     fmap _ ParseError = ParseError
+
+instance Applicative Parser where
+    pure = Parser ""
+
+    Parser _ f  <*> m       = fmap f m
+    ParseError <*> _      = ParseError
