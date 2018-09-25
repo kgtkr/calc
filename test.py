cb6a29b3bee46ee7e3d040e755c158f87aad6d52
kgtkr <kgtkr.jp@gmail.com>
2018-09-25 22:52:35 +0900

    test.py改善

--- test.py
+++ test.py
@@ -2,18 +2,20 @@ import os
 import os.path
 import json
 import subprocess
+import sys
 
 testCases = json.load(open("test.json"))
 
 
 for lang in [x for x in os.listdir(".") if os.path.exists(f"{x}/test.json")]:
-    langJson = json.load(open(f"{lang}/test.json"))
-    print(f"#{lang}")
-    for build in langJson["builds"]:
-        subprocess.run(build, cwd=lang, stdout=subprocess.PIPE)
-    for case in testCases:
-        out = subprocess.run(
-            [case[0] if x == "$ARGS" else x for x in langJson["run"]], cwd=lang,  check=True, stdout=subprocess.PIPE).stdout.strip().decode("UTF-8")
-        if out != case[1]:
-            print(
-                f"Error:\n\tinput:{case[0]}\n\texpect:{case[1]}\n\toutput:{out}")
+    if len(sys.argv) == 1 or sys.argv[1] == lang:
+        langJson = json.load(open(f"{lang}/test.json"))
+        print(f"#{lang}")
+        for build in langJson["builds"]:
+            subprocess.run(build, cwd=lang, stdout=subprocess.PIPE)
+        for case in testCases:
+            out = subprocess.run(
+                [case[0] if x == "$ARGS" else x for x in langJson["run"]], cwd=lang,  check=True, stdout=subprocess.PIPE).stdout.strip().decode("UTF-8")
+            if out != case[1]:
+                print(
+                    f"Error:\n\tinput:{case[0]}\n\texpect:{case[1]}\n\toutput:{out}")
