8c6dc212ad7a3fb9a279de39ab78d3414fe7443a
kgtkr <kgtkr.jp@gmail.com>
2018-09-25 21:43:54 +0900

    main

--- scheme/main.scm
+++ scheme/main.scm
@@ -1,1 +1,7 @@
-(print "Hello World")
+(if (= (length *argv*) 0)
+    ("Error")
+    (parser-error-guard
+        (lambda () (rational->string (parse (string->list (list-ref *argv* 0)))))
+        (lambda () ("Error"))
+    )
+)
