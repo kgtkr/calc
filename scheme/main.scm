fc4689ec5d9697294b388d85481f03554e83a4f7
kgtkr <kgtkr.jp@gmail.com>
2018-09-25 21:45:01 +0900

    バグ修正

--- scheme/main.scm
+++ scheme/main.scm
@@ -1,7 +1,7 @@
-(if (= (length *argv*) 0)
-    ("Error")
-    (parser-error-guard
-        (lambda () (rational->string (parse (string->list (list-ref *argv* 0)))))
-        (lambda () ("Error"))
-    )
+(print (if (= (length *argv*) 0)
+"Error"
+(parser-error-guard
+    (lambda () (rational->string (parse (string->list (list-ref *argv* 0)))))
+    (lambda () "Error")
 )
+))
