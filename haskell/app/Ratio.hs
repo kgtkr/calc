9476e9b5cd140da1e7cff0583f4eef0c7556642c
kgtkr <kgtkr.jp@gmail.com>
2018-09-26 19:20:58 +0900

    Show

--- haskell/app/Ratio.hs
+++ haskell/app/Ratio.hs
@@ -7,4 +7,7 @@ ratio _ 0 = NaN
 ratio n d = Ratio (n `div` g) (d `div` g)
     where g = signum d * gcd (abs n) (abs d)
 
-
+instance Show Ratio where
+    show NaN="NaN"
+    show (Ratio n 1)=show n
+    show (Ratio n d)=(show n)++"/"++(show d)
