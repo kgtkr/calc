24eb8cfd5ae15af6cbfebd52b9a461a707ba8a2d
kgtkr <kgtkr.jp@gmail.com>
2018-09-26 14:53:09 +0900

    Applicative

--- haskell/app/Parser.hs
+++ haskell/app/Parser.hs
@@ -2,19 +2,21 @@ module Parser where
 
 import qualified Data.Bifunctor                as B
 
-data Parser a = Parser (String -> (a,String))|ParseError
+data Parser a = Parser (String -> Maybe (a,String))
 
 instance Functor Parser where
-    fmap f (Parser x) = Parser (\s->(B.first f . x) s)
-    fmap _ ParseError = ParseError
+    fmap f (Parser x) = Parser $ \s->(fmap (B.first f) . x) s
 
 instance Applicative Parser where
-    pure x = Parser (\s->(x,s))
+    pure x = Parser (\s->Just (x,s))
 
-    Parser x  <*> Parser y       = Parser (\s->let (f,s)=x s in (B.first f . y) s)
-    ParseError <*> _      = ParseError
-    _ <*> ParseError      = ParseError
+    Parser x  <*> Parser y       = Parser $ \s->case x s of
+        Just (f,s)->case y s of
+            Just (a,s)->Just (f a,s)
+            Nothing->Nothing
+        Nothing->Nothing
 
 instance Monad Parser where
-    Parser x >>= f = f x
-    ParseError >>= _ =ParseError
+    Parser x >>= f = Parser (\s->let (a,s) = x s in
+        case f a of
+            Parser g->g s)
