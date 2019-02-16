0fb7fad83f625f82305ece0b38ac62aec3d11a0a
kgtkr <kgtkr.jp@gmail.com>
2019-02-16 15:37:59 +0900

    余計なファイル削除

--- test.py
+++ test.py
@@ -9,13 +9,13 @@ testCases = json.load(open("test.json"))
 
 for lang in [x for x in os.listdir(".") if os.path.exists(f"{x}/test.json")]:
     if len(sys.argv) == 1 or sys.argv[1] == lang:
-        langJson = json.load(open(f"{lang}/test.json"))
+        image_name = "calc_"+lang
         print(f"#{lang}")
-        for build in langJson["builds"]:
-            subprocess.run(build, cwd=lang, stdout=subprocess.PIPE)
+        subprocess.run(["docker", "build", "-t", image_name, "."],
+                       cwd=lang, stdout=subprocess.PIPE)
         for case in testCases:
             out = subprocess.run(
-                [case[0] if x == "$ARGS" else x for x in langJson["run"]], cwd=lang,  check=True, stdout=subprocess.PIPE).stdout.strip().decode("UTF-8")
+                ["docker", "run", "-it", "-t", image_name, "./run", case[0]], cwd=lang,  check=True, stdout=subprocess.PIPE).stdout.strip().decode("UTF-8")
             if out != case[1]:
                 print(
                     f"Error:\n\tinput:{case[0]}\n\texpect:{case[1]}\n\toutput:{out}")
